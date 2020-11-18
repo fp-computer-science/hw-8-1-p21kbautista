b808a0ab5b7ab727d42620d3ec202f677ff89282
Kyle Bautista <p21kbautista@fairfieldprep.org>
2020-11-17 22:27:41 -0500

    Add hw8-1

---
+++ hw8-1.py
@@ -0,0 +1,31 @@
+# Kyle Bautista (AMDG) 11/18/2020
+
+# Question 1
+my_list = ['0', '1', '2', '3', '4', '5', '6', '7']
+
+# Question 2
+print(len(my_list))
+
+# Question 3
+print(my_list[3])
+
+# Question 4
+print(5 in (my_list))
+
+# Question 5
+print(10 not in (my_list))
+
+# Question 6
+classmates = ['Azaan', 'Kyle', 'Jack']
+
+# Question 7
+print(classmates[1])
+
+# Question 8
+print(classmates * 3)
+
+# Question 9
+print(my_list + classmates)
+
+# Question 10
+print(len(classmates + my_list))
